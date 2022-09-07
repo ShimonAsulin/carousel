f4e20811373cecd0ec6329fc03716e2b06943d5f
Shimon Asulin <asulin.shimon699@gmail.com>
2022-09-07 10:00:38 +0300

    style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,18 +5,19 @@ const Card = ({ img, price, exclusive }) => {
     <div className="card">
       <img src={img} className="logo-img" />
       <div className="card-details">
-        {exclusive && <img className="exclusive-tag" src={exclusive} />}
-        <h2 className="card-title">No DEPOSIT BONUS</h2>
+        <h2 className="card-title">NO DEPOSIT BONUS</h2>
+        <hr className="card-line" />
         <h5 className="card-precent">400% up to</h5>
         <h3 className="card-price">{price}</h3>
         <h4 className="card-spins">+ 100 Free Spins </h4>
         <h5 className="card-gold">on Cleopatra's Gold</h5>
+        {exclusive && <img className="exclusive-tag" src={exclusive} />}
         <div className="card-section">
         <button className="card-btn">Play</button>
-        <img src="https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/Flag.png" alt="Flag" />
+        <img className="flag" src="https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/Flag.png" alt="Flag" />
         </div>
-        <img src="https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/!.png" alt="" />
       </div>
+        <img className="quastion-mark" src="https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/!.png" alt="" />
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,36 +1,38 @@
 export default [
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/5326ed9854996b9cd3275b2f068aba568423998b/src/asset/images/Logo1.svg",
-    price: "$2000",
-    exclusive: "https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/Exclusive.png"
+    price: "$2,000",
+    exclusive:
+      "https://raw.githubusercontent.com/ShimonAsulin/carousel/8fa4d86eedbd943e08f5d15e09781ecf6f6fa5c1/src/asset/images/Exclusive.svg",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo2.svg",
-    price: "$2000",
+    price: "$4,000",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo3.svg",
-    price: "$2000",
+    price: "$2,000",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo4.svg",
-    price: "$2000",
+    price: "$2,000",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo5.svg",
-    price: "$2000",
+    price: "$2,000",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo6.svg",
-    price: "$2000",
-    exclusive: "https://raw.githubusercontent.com/ShimonAsulin/carousel/master/src/asset/images/Exclusive.png"
+    price: "$2,000",
+    exclusive:
+      "https://raw.githubusercontent.com/ShimonAsulin/carousel/8fa4d86eedbd943e08f5d15e09781ecf6f6fa5c1/src/asset/images/Exclusive.svg",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo7.svg",
-    price: "$2000",
+    price: "$2,000",
   },
   {
     img: "https://raw.githubusercontent.com/ShimonAsulin/carousel/63d85a332a90d152a92747e61c75847923cc602f/src/asset/images/Logo8.svg",
-    price: "$2000",
-  }
+    price: "$2,000",
+  },
 ];
